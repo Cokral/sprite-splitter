0b2d6fc68fe6af5f63aba450aed946915b757033
Cokral <coquereau.thomas@gmail.com>
2024-09-05 13:58:14 +0200

    Add frontend URLs

--- backend/main.py
+++ backend/main.py
@@ -17,6 +17,11 @@ FRONTEND_URL = [
     "https://thomascoquereau.com/",
     "https://Cokral.github.io",
     "https://Cokral.github.io/sprite-splitter/",
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+    "http://localhost:5000",
+    "http://127.0.0.1:5000",
+    "https://sprite-splitter.onrender.com/",
 ]
 app = FastAPI()
 
